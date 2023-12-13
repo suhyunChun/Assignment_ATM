924a3029e07012d27aabab1be3190d23a4af5178
suhyunChun <suhyun.chun@stonybrook.edu>
2023-12-13 16:37:45 +0900

    [Add] add btn click event
    버튼 클릭으로 메뉴 이동 및 특정 오브젝트 보이기 or, 없애기

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager I;
+    public GameObject panel;
+    public GameObject BtnContainer;
+    public GameObject DContainer;
+    public GameObject WContainer;
+    private void Awake()
+    {
+        I = this; //ΩÃ±€≈Ê 
+       
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+    }
+
+    public void GoBack()
+    {
+        panel.SetActive(false);
+    }
+
+    public void ShowDeposit()
+    {
+        BtnContainer.SetActive(false);
+        DContainer.SetActive(true);
+
+    }
+    public void ShowWithDraw()
+    {
+        BtnContainer.SetActive(false);
+        WContainer.SetActive(true);
+    }
+
+    public void GoBackMenu()
+    {
+        DContainer.SetActive(false);
+        WContainer.SetActive(false);
+        BtnContainer.SetActive(true);
+    }
+}
